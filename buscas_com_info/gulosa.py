69d5232bc4d5702edfd444436ca524f89728002f
anafariass <livia.oliveira1.only@gmail.com>
2025-06-24 20:54:58 -0300

    altera chaves de dicionário de 'ESTADO', 'PAI', 'ACAO' e 'CUSTO' para 'estado', 'pai', 'acao' e 'custo' na implementação da busca gulosa

--- buscas_com_info/gulosa.py
+++ buscas_com_info/gulosa.py
@@ -1,26 +1,26 @@
 import heapq
 
 def busca_gulosa(problema, heuristica):
-    no_inicial = {'ESTADO': problema.obter_estado_inicial(), 'PAI': None, 'ACAO': None, 'CUSTO': 0}
+    no_inicial = {'estado': problema.obter_estado_inicial(), 'pai': None, 'acao': None, 'custo': 0}
     
     fronteira = []
-    prioridade = heuristica(no_inicial['ESTADO'], problema)
+    prioridade = heuristica(no_inicial['estado'], problema)
     contador = 0
     heapq.heappush(fronteira, (prioridade, contador, no_inicial))
     
-    explorados = {no_inicial['ESTADO']}
+    explorados = {no_inicial['estado']}
     nos_expandidos = 0
 
     while fronteira:
         _, _, no_atual = heapq.heappop(fronteira)
         nos_expandidos += 1
 
-        if problema.estado_objetivo(no_atual['ESTADO']):
+        if problema.estado_objetivo(no_atual['estado']):
             return no_atual, nos_expandidos
 
-        for estado_sucessor, acao, custo in problema.expandir(no_atual['ESTADO']):
+        for estado_sucessor, acao, custo in problema.expandir(no_atual['estado']):
             if estado_sucessor not in explorados:
-                no_filho = {'ESTADO': estado_sucessor, 'PAI': no_atual, 'ACAO': acao, 'CUSTO': no_atual['CUSTO'] + custo}
+                no_filho = {'estado': estado_sucessor, 'pai': no_atual, 'acao': acao, 'custo': no_atual['custo'] + custo}
                 explorados.add(estado_sucessor)
                 
                 prioridade = heuristica(estado_sucessor, problema)
