27306a7370772a77503660243cf7bbc5b5038c79
arturmartins <arturmartinssilva078@gmail.com>
2025-06-25 11:19:09 -0300

    refactor(dfs): refatora função busca_em_profundidade para melhorar legibilidade e organização do código

--- buscas_sem_infor/dfs.py
+++ buscas_sem_infor/dfs.py
@@ -1,32 +1,40 @@
-
-
 def busca_em_profundidade(problema):
-    no_inicial = {'estado': problema.obter_estado_inicial(),
-                  'pai': None,
-                  'acao': None,
-                  'custo': 0}
+    no_inicial = {
+        'estado': problema.obter_estado_inicial(),
+        'pai': None,
+        'acao': None,
+        'custo': 0
+    }
     
-    fronteira = [no_inicial]
+    if problema.estado_objetivo(no_inicial['estado']):
+        return no_inicial, 0
+    
+    fronteira = [no_inicial] 
     explorados = set()
     nos_expandidos = 0
 
     while fronteira:
         no_atual = fronteira.pop() 
-        nos_expandidos += 1
         
         estado_atual = no_atual['estado']
         
-        if problema.estado_objetivo(estado_atual):
-            return no_atual
+        if estado_atual in explorados:
+            continue
+            
+        explorados.add(estado_atual)
+        nos_expandidos += 1
         
-        if estado_atual not in explorados:
-            explorados.add(estado_atual)
-            for estado_sucessor, acao, custo in reversed(problema.expandir(estado_atual)):
-                no_filho = {'estado': estado_sucessor,
-                            'pai': no_atual,
-                            'acao': acao,
-                            'custo': no_atual['custo'] + custo}
-                
+        if problema.estado_objetivo(estado_atual):
+            return no_atual, nos_expandidos
+        
+        for estado_sucessor, acao, custo in reversed(problema.expandir(estado_atual)):
+            if estado_sucessor not in explorados:
+                no_filho = {
+                    'estado': estado_sucessor,
+                    'pai': no_atual,
+                    'acao': acao,
+                    'custo': no_atual['custo'] + custo
+                }
                 fronteira.append(no_filho)
                 
     return None, nos_expandidos 
