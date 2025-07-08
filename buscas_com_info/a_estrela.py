bcc4dda5d48068d14e389ab8071890ad3c660b83
anafariass <livia.oliveira1.only@gmail.com>
2025-07-08 20:55:23 -0300

    refactor(buscas): melhora a formatação e a legibilidade do código nas funções de busca A* e gulosa

--- buscas_com_info/a_estrela.py
+++ buscas_com_info/a_estrela.py
@@ -1,30 +1,32 @@
 import heapq
 
-def busca_a_estrela(problema, heuristica):
-    no_inicial = {'estado': problema.obter_estado_inicial(), 'pai': None, 'acao': None, 'custo': 0}
+def busca_a_estrela(problema, heuristica): #1
+    no_inicial = {'estado': problema.obter_estado_inicial(),
+                  'pai': None,
+                  'acao': None,
+                  'custo': 0}
     
-    fronteira = []
-    prioridade = no_inicial['custo'] + heuristica(no_inicial['estado'], problema)
-    contador = 0
-    heapq.heappush(fronteira, (prioridade, contador, no_inicial))
-    
-    explorados = {no_inicial['estado']: 0}
+    fronteira = [] #2
+    prioridade = no_inicial['custo'] + heuristica(no_inicial['estado'], problema) # f(n) = g(n) + h(n)
+    contador = 0 
+    heapq.heappush(fronteira, (prioridade, contador, no_inicial)) #4
+    explorados = {no_inicial['estado']: 0}#5
     nos_expandidos = 0
 
-    while fronteira:
-        _, _, no_atual = heapq.heappop(fronteira)
+    while fronteira: #6
+        _, _, no_atual = heapq.heappop(fronteira)#7
         nos_expandidos += 1
 
-        if problema.estado_objetivo(no_atual['estado']):
+        if problema.estado_objetivo(no_atual['estado']):#8
             return no_atual, nos_expandidos
 
-        for estado_sucessor, acao, custo in problema.expandir(no_atual['estado']):
-            novo_custo = no_atual['custo'] + custo
-            if estado_sucessor not in explorados or novo_custo < explorados[estado_sucessor]:
-                explorados[estado_sucessor] = novo_custo
-                prioridade = novo_custo + heuristica(estado_sucessor, problema)
-                no_filho = {'estado': estado_sucessor, 'pai': no_atual, 'acao': acao, 'custo': novo_custo}
+        for estado_sucessor, acao, custo in problema.expandir(no_atual['estado']): #9
+            novo_custo = no_atual['custo'] + custo #10
+            if estado_sucessor not in explorados or novo_custo < explorados[estado_sucessor]: #11
+                explorados[estado_sucessor] = novo_custo #12
+                prioridade = novo_custo + heuristica(estado_sucessor, problema)#13
+                no_filho = {'estado': estado_sucessor, 'pai': no_atual, 'acao': acao, 'custo': novo_custo}#14
                 contador += 1
-                heapq.heappush(fronteira, (prioridade, contador, no_filho))
+                heapq.heappush(fronteira, (prioridade, contador, no_filho)) #15
                 
     return None, nos_expandidos 
--- buscas_com_info/gulosa.py
+++ buscas_com_info/gulosa.py
@@ -1,34 +1,37 @@
 import heapq
 
 def busca_gulosa(problema, heuristica):
-    no_inicial = {'estado': problema.obter_estado_inicial(), 'pai': None, 'acao': None, 'custo': 0} # eh como o no eh representado
+    no_inicial = {'estado': problema.obter_estado_inicial(),
+                  'pai': None,
+                  'acao': None,
+                  'custo': 0} 
      
     fronteira = []
-    prioridade = heuristica(no_inicial['estado'], problema)
+    prioridade = heuristica(no_inicial['estado'], problema) #1
     contador = 0
     heapq.heappush(fronteira, (prioridade, contador, no_inicial))
     
-    explorados = {no_inicial['estado']}
+    explorados = {no_inicial['estado']} #2
     nos_expandidos = 0
 
     while fronteira:
-        _, _, no_atual = heapq.heappop(fronteira)
+        _, _, no_atual = heapq.heappop(fronteira) #3
         nos_expandidos += 1
 
-        if problema.estado_objetivo(no_atual['estado']):
+        if problema.estado_objetivo(no_atual['estado']):#3
             return no_atual, nos_expandidos
 
-        for estado_sucessor, acao, custo in problema.expandir(no_atual['estado']):
-            if estado_sucessor not in explorados:
+        for estado_sucessor, acao, custo in problema.expandir(no_atual['estado']): #4
+            if estado_sucessor not in explorados:#5
                 no_filho = {'estado': estado_sucessor,
                             'pai': no_atual, 
                             'acao': acao, 
                             'custo': no_atual['custo'] + custo}
                 
-                explorados.add(estado_sucessor)
+                explorados.add(estado_sucessor) #6
                 
-                prioridade = heuristica(estado_sucessor, problema)
+                prioridade = heuristica(estado_sucessor, problema) #7
                 contador += 1
-                heapq.heappush(fronteira, (prioridade, contador, no_filho))
+                heapq.heappush(fronteira, (prioridade, contador, no_filho))#8
                 
     return None, nos_expandidos
